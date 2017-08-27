95d356fd5d7329538ff109ff4912bf697941a6f2
akshaykakad <kakadakshay@gmail.com>
2017-08-27 22:53:08 +0530

    Add photos activity added.

--- app/src/main/java/com/hfad/photostory/CreateEditActivity.java
+++ app/src/main/java/com/hfad/photostory/CreateEditActivity.java
@@ -2,6 +2,8 @@ package com.hfad.photostory;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
 
 public class CreateEditActivity extends Activity {
 
@@ -9,5 +11,10 @@ public class CreateEditActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_edit);
+    }
+
+    public void onAddPhotosClick(View view){
+        Intent intent = new Intent(this, AddPhotosActivity.class);
+        startActivity(intent);
     }
 }
